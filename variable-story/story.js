7c904fd280e9c1c7adc8c28efd06e202a21da2c5
susukinokenwa <118701401+susukinokenwa@users.noreply.github.com>
2023-01-10 14:56:59 +0800

    1.2

--- variable-story/story.js
+++ variable-story/story.js
@@ -5,9 +5,38 @@ document.write(`<p>once upon the time, there was a catapillar called ${name}.</p
 
 document.write(`<p>And after it  was born, it was very hungry.</p>`)
 
-document.write(` "I'm so hungry, I need some food to eat" ${name} said. `)
-
+document.write(`    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"I'm so hungry, I need some food to eat" ${name} said. </p>`)
+document.write(`<p>The catapillar moved slowly to look for some food.</p>`)
 const food1 = prompt("favorite junk food?")
 
-document.write(` <p>On the first day, he ate a ${food1} </p>`)
-document.write(`<p>"I'm still hungry, I need more food." said the catapillar</p>`)
+document.write(` <p>On the first day, it ate a ${food1} </p>`)
+document.write(`<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"Yummy! I love to have more food tomorrow" said the catapillar</p>`)
+
+const food2 = prompt("What is your least favorite food?")
+const day = prompt("What is the day after Monday?")
+
+document.write(`On ${day} he ate two ${food2}s. "Ummmmmm, the pears are delicious, but … I am still hungry." Said ${name}.`)
+
+const food3 = prompt(" What is your favorite vegatables?")
+
+const food4 = prompt("What is your favorite fruit?")
+
+document.write(`<p>On the next day it ate through three ${food3}.</p><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Caterpillar:Why am I still hungry? Foooood! </p>`)
+
+document.write(`<p>On Friday, he ate through... one piece of chocolate cake, one ${food3}, One slice of Swiss cheese,</p><p> and One slice of salami.
+One ${food1}, one piece of cherry pie, one ${food2}, one ${food4}, and one slice of watermelon.</p>`)
+
+document.write(`<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"Ouch! My stomach hurts." said the catapillar.</p>`)
+
+document.write(`That night he had a stomachache! The next day was Sunday again. ${name} ate through one nice green leaf, and after that, he felt much better.`)
+
+document.write(`<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"Finally, my stomach is much better." Said${name}</p>`)
+
+document.write(`Now he was not hungry, and he was not a little caterpillar anymore.</p><p>
+He was a big, fat ${name}.He built a small house, called a cocoon, </p><p>around himself. He stayed inside for more than two weeks.</p><p>
+Then he nibbled a hole in the cocoon, pushed his way out...`)
+
+document.write(`<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"What a lovely world" said the catapillar</p><p>Now ${name} was a beautiful butterfly!</p>`)
+
+document.write(`<img src="https://d7hftxdivxxvm.cloudfront.net/?height=567&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fd_r07DtQ_Tz4VS87YK-LAA%2Fnormalized.jpg&width=800">`)
+
