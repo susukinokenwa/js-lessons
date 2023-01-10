8fbd2d0b8a01b8b97353fc41a3a61178dee81740
susukinokenwa <118701401+susukinokenwa@users.noreply.github.com>
2023-01-10 10:10:17 +0800

    1.1

---
+++ lessons/lesson-js.js
@@ -0,0 +1,58 @@
+
+//variables
+
+//permanent
+const age = 15
+const year = 2023
+
+//changable
+let favnum = 0
+
+
+//datatypes
+const student1 = "skrler"
+let student2 = "student"
+
+//contactenation (joining things together)
+    //using +
+document.write("<p class='first-message'>Hi</p> "+ student1)
+    //using``
+document.write(`${student1} is born in ${favnum} and she is a ${student2}.`)
+
+//prompts
+const name = prompt("what is your name")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ variable-story/story.js
@@ -0,0 +1,13 @@
+//prompts
+const name = prompt("name?")
+
+document.write(`<p>once upon the time, there was a catapillar called ${name}.</p>` )
+
+document.write(`<p>And after it  was born, it was very hungry.</p>`)
+
+document.write(` "I'm so hungry, I need some food to eat" ${name} said. `)
+
+const food1 = prompt("favorite junk food?")
+
+document.write(` <p>On the first day, he ate a ${food1} </p>`)
+document.write(`<p>"I'm still hungry, I need more food." said the catapillar</p>`)
