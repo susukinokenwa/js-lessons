e6a91ddac5caf7b4f2c8f9109f930dd8d63e8125
susukinokenwa <118701401+susukinokenwa@users.noreply.github.com>
2023-02-08 11:57:47 +0800

    2.

---
+++ function/lesson4.js
@@ -0,0 +1,27 @@
+//functions
+    //1. defining
+    //2. calling
+
+
+
+
+//define
+
+function alleyOop(){
+    document.write(`Anya got the ball`)
+    document.write(`Beki made a great pass`)
+    document.write(`Anya dunked over you`)
+    console.log(true)
+}
+
+
+//call
+
+alleyOop()
+alleyOop()
+
+
+function hurray(){
+    document.write(` Anya and Beki cheered`)
+    console.log(`hurray!`)
+}
