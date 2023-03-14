2499786c56acffd9e8af9a5def58da1f0d2dfeae
susukinokenwa <118701401+susukinokenwa@users.noreply.github.com>
2023-03-14 10:41:51 +0800

    1.0

---
+++ object/lesson8.js
@@ -0,0 +1,61 @@
+
+//Data types
+
+const  student = "Anya" //string
+const age = 9 //number
+const graduated = false //boolean
+const wpm = [23, 323, 32] //array
+
+
+
+//object
+
+const dolphin = {
+
+    //property descriptions
+    color: "blue", 
+    long: 40, 
+    habitat: "ocean",
+
+
+    //methods (action)
+
+    swim(){
+        console.log("it swam...")
+    },
+    eat(){
+        console.log("It ate a wahle!?")
+    }
+}
+
+
+const cindy = {
+    hair: "short",
+    glass: "noGlassses",
+    height: 160,
+
+    walk(){
+        console.log("She walked...")
+    }
+
+}
+const rola = {
+    hair:"long",
+    height:150,
+    status:"basketballCoach",
+
+    playBasketball(){
+        console.log("She got the ball!")
+    }
+}
+const kristina = {
+    hair: "long",
+    height: 160,
+    glass:"noGlassses",
+
+    laugh(){
+        console.log("She laughed...")
+    }
+}
+
+
